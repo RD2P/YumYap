58399a8b8ff54b8b25505c23f42dad2809badd7c
Raphael <graphael.dr@gmail.com>
2024-01-13 00:39:33 -0600

    Display markdown as html

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,32 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Recipe from './components/Recipe'
 import Poutine from './components/Poutine'
-
+import { useState, useEffect } from 'react'
+import Markdown from 'react-markdown'
 
 function App() {
+
+  const [recipe, setRecipe] = useState('')
+  useEffect(() => {
+    import("./md/poutine.md")
+      .then(res => {
+        fetch(res.default)
+          .then(res => res.text())
+          .then(res => setRecipe(res))
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <>
       <Header />
+
+
       <div className='max-w-7xl mx-auto'>
+      <Markdown>{recipe}</Markdown>
+
         {/* <Recipe /> */}
-        <Poutine />
+        {/* <Poutine /> */}
       </div>
       <Footer />
     </>
