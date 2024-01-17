9183511a8e1053df7e413c1812a0074a70f24dfe
Raphael <graphael.dr@gmail.com>
2024-01-17 16:32:59 -0600

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex justify-center py-9 mt-10 shadow-lg bg-primary'>
-      <div className=''>YumYap @2024</div>
+      <div className=''>YumYap @{new Date().getFullYear()}</div>
     </div>
   )
 }
