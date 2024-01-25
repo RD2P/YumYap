9eaab636611aa239e75db293f742e4353c50fe0d
Raphael <graphael.dr@gmail.com>
2024-01-24 23:28:37 -0600

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center py-9 mt-10 shadow-lg bg-primary'>
-      <div className=''>YumYap @{new Date().getFullYear()}</div>
+    <div className='flex justify-center py-9 mt-10 shadow-lg bg-primary text-white'>
+      <div className=''>Copyright @ {new Date().getFullYear()} YumYap</div>
     </div>
   )
 }
